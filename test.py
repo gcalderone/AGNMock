785fa2770fd0df0ad057b594c84165ac3d0c942f
Giorgio Calderone <giorgio.calderone@gmail.com>
2021-10-05 15:33:33 +0200

    Added comments

--- test.py
+++ test.py
@@ -6,13 +6,56 @@ import AGNModel as m
 # Create an AGNModel object
 agn = m.AGNModel()
 
+
 # --------------------------------------------------------------------
-# Plot intrinsic spectrum
+# Set model properties
+
+# BH mass
+agn.disk.bh_logmass = 9.         # log10(M / M_sun)
+
+# Accretion disk inner radius (used to calculate radiative efficiency
+agn.disk.rin = 3.                # gravitational radius: 6:Schwarzschild, 1:Maximally rotating, 9:Maximally counter-rotating
+
+# Disk emitted bolometric luminosity can be specified using one of the
+# following methods (the other quantities will be automatically
+# re-calculated):
+agn.disk.eddington_ratio = 0.01  # must be in the range (0,1)
+agn.disk.accrate = 1.            # accretion rate, M_sun yr^-1
+agn.disk.luminosity = 5e45       # erg / s
+
+# Broad line region
+agn.blr.luminosity = 1.5e41      # Luminosity of Ly-alpha [erg s^-1] (the remaining lines are scaled accordingly)
+agn.blr.fwhm  = 6e3              # FWHM of all lines in BLR [km s^-1]
+
+# Narrow line region
+agn.nlr.luminosity = 1.5e41      # Luminosity of Ly-alpha [erg s^-1] (the remaining lines are scaled accordingly)
+agn.nlr.fwhm  = 6e2              # FWHM of all lines in NLR [km s^-1]
+
+# Host galaxy
+agn.host.template = 'Ell5'       # Choose one from the SWIRE collection (http://www.iasf-milano.inaf.it/~polletta/templates/swire_templates.html)
+agn.host.luminosity = 5e43       # νL_ν luminosity @ 5500Å
+
+# Torus
+agn.torus.luminosity = 2e44      # total integrated luminosity [erg s^-1], typically ~1/3 of the disk bolometric luminosity
+
+
+# The model already comes with pre-defined values when it is created.
+# Such values can either be changed (as shown above) or left at their
+# default values.  The latter can be restored at any time with:
+#
+# agn.defaultValues()
+
+
+
+# --------------------------------------------------------------------
+# Plot intrinsic (emitted) spectrum
 plt.title("Intrinsic spectrum")
 plt.xlabel("Wavelength [A] (rest frame)")
 plt.ylabel("Luminosity [erg s^{-1}]")
 
+# Prepare a grid of wavelengths to evaluate the model
 wl = np.logspace(2, 7, 5000)
+
 total = agn.spectrum(wl)
 mm = np.amax(wl * total)
 plt.ylim((mm / 1000, mm * 2))
@@ -23,15 +66,20 @@ p1, = plt.loglog(wl, wl * total, label="Total")
 plt.legend(handles=[p1,p2,p3,p4])
 plt.show()
 
+
 # --------------------------------------------------------------------
 # Plot observed spectrum
 plt.title("Observed spectrum")
 plt.xlabel("Wavelength [A] (observer frame)")
 plt.ylabel("Flux [erg s^{-1} cm^{-2}]")
 
+# Place the source at z=2 with a disk inclination of 30 degrees w.r.t
+# the line of sight (a pole on line of sight corresponds to 0 degrees)
 agn.observe(2., 30.)
 
+# Prepare a grid of wavelengths to evaluate the model
 wl = np.logspace(1, 8, 5000)
+
 total = agn.spectrum(wl)
 mm = np.amax(wl * total)
 plt.ylim((mm / 1000, mm * 2))
